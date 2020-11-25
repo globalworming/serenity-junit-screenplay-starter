74f8da739f46c9264606643f54da17e86976b092
globalworming <aw_github@headissue.com>
2020-11-25 14:10:56 +0100

    make sure people can test the browser

--- src/test/java/com/example/e2e/browser/BrowserIT.java
+++ src/test/java/com/example/e2e/browser/BrowserIT.java
@@ -3,10 +3,15 @@ package com.example.e2e.browser;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.serenitybdd.screenplay.actions.Open;
 import net.thucydides.core.annotations.Managed;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
+import static org.hamcrest.CoreMatchers.containsString;
 
 @RunWith(SerenityRunner.class)
 public class BrowserIT {
@@ -18,5 +23,17 @@ public class BrowserIT {
   public void whenDoingAnExample() {
     Actor tester = new Actor("tester");
     tester.can(BrowseTheWeb.with(aBrowser));
+    tester.attemptsTo(Open.url("https://example.com"));
+    tester.should(seeThat(a -> BrowseTheWeb.as(a).getDriver().findElement(By.cssSelector("body")).getText(), containsString("Example Domain")));
   }
+
+  @Test
+  public void whenWeNeedAMission() {
+    // accessing a test report:
+    // navigate to report page https://lemon-desert-049177e03.azurestaticapps.net/
+    // find the quicklinks by clicking the filter button
+    // click "trace errors"
+    // ensure 9 test results are shown
+  }
+
 }
