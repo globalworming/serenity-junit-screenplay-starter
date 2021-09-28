72aeeaf0ff1475a5a55d63f9619fd9cadca8c584
globalworming <andreas.worm@headissue.com>
2021-09-28 23:54:34 +0200

    Revert "migrate to junit5"
    This reverts commit 141ee6f1

--- src/test/java/com/example/e2e/api/JsonPlaceHolderIT.java
+++ src/test/java/com/example/e2e/api/JsonPlaceHolderIT.java
@@ -3,37 +3,40 @@ package com.example.e2e.api;
 import com.example.screenplay.action.GetAListOfAllPosts;
 import com.example.screenplay.action.UploadNewPost;
 import com.example.screenplay.question.NumberOfReturnedPosts;
+import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.rest.abilities.CallAnApi;
 import net.thucydides.core.annotations.Narrative;
 import org.apache.http.HttpStatus;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 import static net.serenitybdd.screenplay.rest.questions.ResponseConsequence.seeThatResponse;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.greaterThan;
 
+@RunWith(SerenityRunner.class)
 @Narrative(title = "title for json placeholder tests", text = "FIXME description")
-class JsonPlaceHolderIT {
+public class JsonPlaceHolderIT {
 
   private static Actor author;
 
-  @BeforeAll
-  static void setUp() {
+  @BeforeClass
+  public static void setUp() {
     author = new Actor("author");
     author.can(CallAnApi.at("https://jsonplaceholder.typicode.com"));
   }
 
   @Test
-  void whenThereAreManyPosts() {
+  public void whenThereAreManyPosts() {
     author.attemptsTo(new GetAListOfAllPosts());
     author.should(seeThat(new NumberOfReturnedPosts(), greaterThan(99)));
   }
 
   @Test
-  void whenPosting() {
+  public void whenPosting() {
     author.attemptsTo(UploadNewPost.containing("{\"title\":\"foo\",\"body\":\"bar\",\"userId\":1}"));
 
     author.should(seeThatResponse(response -> response.statusCode(HttpStatus.SC_CREATED)));
--- src/test/java/com/example/e2e/browser/ReportNavigatorIT.java
+++ src/test/java/com/example/e2e/browser/ReportNavigatorIT.java
@@ -3,23 +3,26 @@ package com.example.e2e.browser;
 import com.example.screenplay.action.AccessTheLatestReport;
 import com.example.screenplay.action.LookUpAllUnsuccessfulOutcomes;
 import com.example.screenplay.question.OutcomesShown;
+import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.actors.OnlineCast;
 import net.serenitybdd.screenplay.questions.CountQuestion;
 import net.thucydides.core.annotations.Narrative;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static net.serenitybdd.screenplay.EventualConsequence.eventually;
 import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 import static org.hamcrest.CoreMatchers.is;
 
 @Narrative(text = "describes the report behavior")
-class ReportNavigatorIT {
+@RunWith(SerenityRunner.class)
+public class ReportNavigatorIT {
 
   private OnlineCast cast = new OnlineCast();
 
   @Test
-  void whenTracingErrors() {
+  public void whenTracingErrors() {
     Actor tester = cast.actorUsingBrowser("chrome").named("tester");
     tester.attemptsTo(new AccessTheLatestReport());
     tester.attemptsTo(new LookUpAllUnsuccessfulOutcomes());
