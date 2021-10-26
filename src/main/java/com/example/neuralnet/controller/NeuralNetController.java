f7f6042ecaa0a49a3f87651f74fc0c8b4fe244eb
globalworming <andreas.worm@headissue.com>
2021-10-26 22:22:24 +0200

    add frontend

--- src/main/java/com/example/neuralnet/controller/NeuralNetController.java
+++ src/main/java/com/example/neuralnet/controller/NeuralNetController.java
@@ -1,14 +1,13 @@
 package com.example.neuralnet.controller;
 
 import com.example.neuralnet.component.NeuralNetwork;
-import com.example.neuralnet.domain.InferenceResult;
+import com.example.neuralnet.domain.InferenceResults;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,8 +16,8 @@ public class NeuralNetController {
   private final NeuralNetwork neuralNetwork;
 
   @GetMapping("/infer")
-  List<InferenceResult> infer(@RequestParam Double color) {
-    return Collections.singletonList(neuralNetwork.infer(color));
+  InferenceResults infer(@RequestParam Double color) {
+    return InferenceResults.builder().inferenceResults(Collections.singletonList(neuralNetwork.infer(color))).build();
   }
 
   @GetMapping("/train")
---
+++ src/react/src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+import React, {useEffect, useState} from 'react';
+import Button from 'react-bootstrap/Button';
+
+import {HexColorPicker} from "react-colorful";
+
+function App() {
+  return (
+      <div className="App">
+        <header className="App-header">
+          <ColorPicker/>
+        </header>
+      </div>
+  );
+}
+
+export default App;
+
+function approximateBlackness(color) {
+  console.log(color.toString(16), (color % 0x100), (color % 0x100) / 0xFF);
+  return ((color % 0x100) / 0xFF);
+}
+
+const ColorPicker = () => {
+  const [color, setColor] = useState(0xaabbcc);
+  const [result, setResult] = useState([]);
+
+  useEffect(() => {
+    fetch('/infer?color=' + approximateBlackness(color), {mode: 'no-cors'})
+        .then(response => response.json())
+        .then(data => setResult(data['inferenceResults']));
+  }, [color, setResult]);
+
+  const updateColor = (color) => {
+    setColor(parseInt(color.substring(1), 16))
+  }
+
+  let colorToString = "#" + color.toString(16);
+  return <>
+    <HexColorPicker color={colorToString} onChange={updateColor}/>
+    <p>
+      <span>{JSON.stringify(result[0], null, 2)}</span>
+      <Button onClick={() => fetch('/train?label=black')}>reward for black</Button>
+    </p>
+  </>;
+};
