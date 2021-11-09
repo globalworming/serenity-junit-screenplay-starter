dce0fb76e15099fc31fc862fb042b00031a107d0
globalworming <andreas.worm@headissue.com>
2021-11-09 22:56:38 +0100

    fix test

--- src/test/java/com/example/screenplay/question/browser/TheMostLikelyLabelIsOnTop.java
+++ src/test/java/com/example/screenplay/question/browser/TheMostLikelyLabelIsOnTop.java
@@ -6,7 +6,6 @@ import net.serenitybdd.core.pages.WebElementFacade;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.targets.Target;
 import org.hamcrest.CoreMatchers;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,18 +15,16 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class TheMostLikelyLabelIsOnTop extends QuestionWithDefaultSubject<Boolean> {
   @Override
   public Boolean answeredBy(Actor actor) {
-    val confidenceValues = Target.the("confidence values").locatedBy(byE2eTestId("inference-results")).resolveAllFor(actor).stream().map(WebElementFacade::getText).collect(Collectors.toList());
+    val confidenceValues =
+        Target.the("confidence values").locatedBy(".e2e-inference-results dd").resolveAllFor(actor)
+            .stream()
+            .map(WebElementFacade::getText)
+            .collect(Collectors.toList());
     val actualHighestValue = Double.parseDouble(confidenceValues.get(0));
-    List<Double> displayedValues = confidenceValues.stream().map(Double::parseDouble).sorted().collect(Collectors.toList());
+    List<Double> displayedValues =
+        confidenceValues.stream().map(Double::parseDouble).sorted().collect(Collectors.toList());
     val expectedHighestValue = displayedValues.get(displayedValues.size() - 1);
     assertThat(actualHighestValue, CoreMatchers.is(expectedHighestValue));
     return true;
-  }
-
-  @NotNull
-  private String byE2eTestId(String s) {
-    return "//*[@data-e2e-test-id=\"" +
-        s +
-        "\"]/dd";
   }
 }
