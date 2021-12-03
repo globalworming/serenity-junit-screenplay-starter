f1175da1473869ae101e3e9b2cf7dfff41e5428a
globalworming <andreas.worm@headissue.com>
2021-12-03 21:58:54 +0100

    update version, reword

--- src/test/java/com/example/e2e/api/package-info.java
+++ src/test/java/com/example/e2e/api/package-info.java
@@ -1,5 +1,8 @@
-@Narrative(title = "API Checks", text={
-    "List of Rest API examples",
-})
+@Narrative(
+    title = "API Checks",
+    text = {
+      "Some http client examples. Serenity comes with a wrapper for RestAssured but you can totally just use whatever client you want... given you want to implement all the tedious thing yourself.",
+    })
 package com.example.e2e.api;
+
 import net.thucydides.core.annotations.Narrative;
