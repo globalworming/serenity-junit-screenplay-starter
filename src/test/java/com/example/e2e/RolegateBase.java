0f91998f1607d44e287c787983b286f52aebcd44
globalworming <andreas.worm@headissue.com>
2021-12-03 23:56:17 +0100

    move baseclass else it wont show up in the requirements report

--- src/test/java/com/example/e2e/RolegateBase.java
+++ src/test/java/com/example/e2e/RolegateBase.java
@@ -1,19 +1,22 @@
-package com.example;
+package com.example.e2e;
 
 import com.example.screenplay.ability.PerformHttpsRequests;
 import com.example.screenplay.actor.Memory;
 import com.github.javafaker.Faker;
 import com.github.javafaker.FunnyName;
+import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.thucydides.core.annotations.Managed;
 import okhttp3.OkHttpClient;
 import org.junit.Before;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 
 import java.util.Collections;
 import java.util.List;
 
+@RunWith(SerenityRunner.class)
 public class RolegateBase {
   protected Actor gameMaster;
   protected List<Actor> players;
