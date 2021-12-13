aa9ff0b98595e4007d82154ca51c0e4ed4a4be91
globalworming <andreas.worm@headissue.com>
2021-12-13 21:49:16 +0100

    formatting, cleanup

--- src/react/src/Actions.js
+++ src/react/src/Actions.js
@@ -1,37 +1,35 @@
-function buildInferQueryPart({h, s, l}) {
+function buildInferQuery({h, s, l}) {
   return `h=${h}&s=${s}&l=${l}`;
 }
 
-function buildRememberQueryPart({h, s, l}, label) {
+function buildRememberQuery({h, s, l}, label) {
   return `h=${h}&s=${s}&l=${l}&label=${label}`;
 }
 
-
 const Actions = {
-  askForInferenceResults: (hsl, callback) => {
-    return fetch('/infer?' +
-        buildInferQueryPart(hsl), {mode: 'no-cors'})
-        .then(response => response.json())
-        .then(callback);
-  },
-
-  askForModel: (callback) => fetch('/model', {mode: 'no-cors'})
+  askForInferenceResults: (hsl, callback) => fetch('/infer?' + buildInferQuery(hsl))
       .then(response => response.json())
       .then(callback),
 
-  askForCurrentError: (callback) => fetch('/currentError', {mode: 'no-cors'})
+  askForModel: (callback) => fetch('/model')
+      .then(response => response.json())
+      .then(callback),
+
+  askForCurrentError: (callback) => fetch('/currentError')
       .then(response => response.text())
       .then(result => callback(result))
   ,
 
-  askForFacts: (callback) => fetch('/facts', {mode: 'no-cors'})
+  askForFacts: (callback) => fetch('/facts')
       .then(response => response.json())
       .then(callback),
 
 
-  doEstablishFact: (label, hsl) => fetch('/remember?' + buildRememberQueryPart(hsl, label), {mode: 'no-cors'}),
-  doReset: () => fetch('/reset', {mode: 'no-cors'}),
-  doTrain: () => fetch('/train', {mode: 'no-cors'})
+  doEstablishFact: (label, hsl) => fetch('/remember?' + buildRememberQuery(hsl, label)),
+
+  doReset: () => fetch('/reset'),
+
+  doTrain: () => fetch('/train')
 
 };
 export default Actions
