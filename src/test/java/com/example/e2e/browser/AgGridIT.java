f5e50096f9ef2ea4379fcaa0658d503ac5acecfb
globalworming <andreas.worm@headissue.com>
2022-06-17 12:03:50 +0200

    cleanup

--- src/test/java/com/example/e2e/browser/AgGridIT.java
+++ src/test/java/com/example/e2e/browser/AgGridIT.java
@@ -5,7 +5,6 @@ import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.serenitybdd.screenplay.actions.Open;
 import net.serenitybdd.screenplay.actors.OnlineCast;
-import net.thucydides.core.annotations.Narrative;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.By;
@@ -15,23 +14,21 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
 @ExtendWith(SerenityJUnit5Extension.class)
-public class ScrollIT {
+public class AgGridIT {
 
   private OnlineCast cast = new OnlineCast();
 
   @Test
-  public void whenScrollingRight() {
+  public void whenJumpingRight() {
     Actor tester = cast.actorUsingBrowser("chrome").named("tester");
     tester.attemptsTo(Open.url("https://www.ag-grid.com/example/"));
 
     WebDriver driver = BrowseTheWeb.as(tester).getDriver();
     driver.findElement(By.cssSelector(".ag-center-cols-container .ag-cell[col-id=\"name\"]") ).click();
     Actions actions = new Actions(driver);
-    WebElement element = driver.findElement(By.cssSelector(".ag-center-cols-container .ag-cell[col-id=\"name\"]") );
-    Actions scrollDown = actions.moveToElement( element );
-    scrollDown.keyDown(Keys.SHIFT).sendKeys(Keys.END).build().perform();
+    actions.keyDown(Keys.SHIFT).sendKeys(Keys.END).build().perform();
 
-    //scrollDown.sendKeys(Keys.ARROW_RIGHT, Keys.ARROW_RIGHT, Keys.ARROW_RIGHT, Keys.ARROW_RIGHT,Keys.ARROW_RIGHT, Keys.ARROW_RIGHT, Keys.ARROW_RIGHT).build().perform();
+    //actions.sendKeys(Keys.ARROW_RIGHT, Keys.ARROW_RIGHT, Keys.ARROW_RIGHT, Keys.ARROW_RIGHT,Keys.ARROW_RIGHT, Keys.ARROW_RIGHT, Keys.ARROW_RIGHT).build().perform();
 
   }
 }
